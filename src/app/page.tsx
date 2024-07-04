9003bdb1ef7860b7d55ee26278caac8b709dfd54
sobbingtofu <kukubasak@naver.com>
2024-07-04 16:15:54 +0900

    Add: tanstack query 패키지 설치

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
-import Image from "next/image"
+"use client"
+
+import { useEffect } from "react"
+import axios from "axios"
 
 export default function Home() {
+  const fecthData = async () => {
+    const { data } = await axios.get("http://localhost:3000/api/pokemons")
+    console.log(data)
+  }
+  useEffect(() => {
+    fecthData()
+  })
+
   return <div className="min-h-screen bg-slate-600">asdfasd</div>
 }
